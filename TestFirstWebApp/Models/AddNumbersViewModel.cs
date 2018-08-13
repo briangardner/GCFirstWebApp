cfc9c5d82901a87936d9e73f577f4e55a14ad1ed
Brian Gardner <1761566+briangardner@users.noreply.github.com>
2018-08-13 13:26:31 -0400

    Initial Commit

---
+++ TestFirstWebApp/Models/AddNumbersViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestFirstWebApp.Models
+{
+    public class AddNumbersViewModel
+    {
+        public int Number1 { get; set; }
+        public int Y { get; set; }
+        public int Result => Number1 + Y;
+    }
+}
