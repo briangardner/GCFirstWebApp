6a4323741dfb2ed63acd187c90aafa9a058c71db
Brian Gardner <1761566+briangardner@users.noreply.github.com>
2018-08-13 16:02:02 -0400

    Added some jquery validation.

--- TestFirstWebApp/Models/AddNumbersViewModel.cs
+++ TestFirstWebApp/Models/AddNumbersViewModel.cs
@@ -7,6 +7,8 @@ namespace TestFirstWebApp.Models
 {
     public class AddNumbersViewModel
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
         public int Number1 { get; set; }
         public int Y { get; set; }
         public int Result => Number1 + Y;
